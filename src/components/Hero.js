f80a367205ae29f3b87c65395b4c4bf13ab3fea3
Alexander Castillo <jhuiicebox@gmail.com>
2022-09-12 19:41:14 -0600

    added hero page and banner

---
+++ src/components/Hero.js
@@ -0,0 +1,25 @@
+
+const content = require('../content.json');
+
+const Hero = () => {
+  return (
+    <section id="hero">
+      <div className="heroContainer">
+        <p class="heroIntro">{content.intro}</p>
+        <h1 class="hero top">{content.name}</h1>
+        <h1 class="hero bottom">{content.statement}</h1>
+        <p class="heroText">{content.heroText}</p>
+        <button className="heroButton">
+          <a
+          href="https://www.github.com/alexjohn7516"
+          target="_blank"
+          rel="noreferrer">
+            Check Out My Projects
+          </a>
+        </button>
+      </div>
+    </section>
+  )
+}
+
+export default Hero;
