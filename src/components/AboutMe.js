9766279379354f336649e6647f60034da2ab3342
Alexander Castillo <jhuiicebox@gmail.com>
2022-09-13 17:09:45 -0600

    added about me w/ @media

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,32 +1,54 @@
 import styles from "../styles/AboutMe.module.css";
-
+import arrow from "../assets/icons/arrow.svg";
+import self from "../assets/images/self-portrait-min.jpg";
 
 const AboutMe = () => {
+
+
+  const technologies =
+    [
+      "JavaScript (ES6+)",
+      "Python",
+      "React",
+      "Node.js",
+      "Django",
+      "SQL"
+  ];
+
   return (
-    <section id="aboutme">
-      <div>
+    <section id={styles.aboutme}>
+      <div className={styles.container}>
         <div className={styles.heading}>
-          <h2><span>I.</span>About Me</h2>
+          <h2><span>I.</span> About Me</h2>
           {/* <Line /> */}
         </div>
         <div className={styles.body}>
           <div className={styles.description}>
             <div className={styles.text}>
-
+              <p>I am a self-taught software enthusiast creating frontend, data driven products. My enjoyment for web development started when I took an online python EDx course to get ahead in school, I was hooked after that.</p>
+              <p>From that first moment of development I have learned many software languages and frameworks. My main focus is to learn professional workflows and build user interactable and data driven projects.</p>
+              <p>My goal is to secure a career as a front end software developer, and further learn a backend development to satisfy my desire to learn.</p>
+              <p>These are a few technologies I have been working with recently.</p>
             </div>
             <div>
-              <ul classHanem={styles.technologies}>
-                <li><span className={styles.arrow}><image src="../assets/icons/arrow.svg" alt="arrow" /></span>JavaScript (ES6+)</li>
-                <li>Python</li>
-                <li>Django</li>
-                <li>SQL</li>
-                <li>React</li>
-                <li>Node.js</li>
+              <ul className={styles.technologies}>
+                {technologies.map((tech) => {
+                  return (
+                    <li>
+                      <div>
+                        <span className={styles.arrow}>
+                          <img src={arrow} alt="arrow poiting right" />
+                        </span>
+                        <p>{tech}</p>
+                      </div>
+                    </li>
+                  )
+                })}
               </ul>
             </div>
           </div>
-          <div className={styles.image}>
-
+          <div className={`${styles.image} ${styles.portrait}`}>
+            <img src={self} alt="Professional head shot of my self" />
           </div>
         </div>
       </div>
