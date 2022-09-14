d5183219dd68c14e9f84432eaea99e953332a45f
Alexander Castillo <jhuiicebox@gmail.com>
2022-09-14 12:17:44 -0600

    created more projects section

---
+++ src/assets/content/moreProjects.js
@@ -0,0 +1,32 @@
+export const moreProjects = [
+  {
+    title: "Scalping Bot",
+    description: "Scalping bot with maximum ML skills to pull any data",
+    url: "https://www.google.com",
+  },
+  {
+    title: "Coming Soon",
+    description: "A fancy projects for you coming soon",
+    url: "",
+  },
+  {
+    title: "Coming Soon",
+    description: "A fancy projects for you coming soon",
+    url: "",
+  },
+  {
+    title: "Coming Soon",
+    description: "A fancy projects for you coming soon",
+    url: "",
+  },
+  {
+    title: "Coming Soon",
+    description: "A fancy projects for you coming soon",
+    url: "",
+  },
+  {
+    title: "Coming Soon",
+    description: "A fancy projects for you coming soon",
+    url: "",
+  }
+]
