4d6b8554e56d1aacaf06a44160bff7cc7a55faae
Alexander Castillo <jhuiicebox@gmail.com>
2022-09-14 21:28:35 -0600

    fixed overflow for hero

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -6,7 +6,7 @@ const Hero = () => {
       <div className={styles.heroContainer}>
         <p className={styles.intro}>Hi, My Name is...</p>
         <h1 className={styles.heading}>Alexander Castillo.</h1>
-        <h1 className={styles.heading}>I Build things for the web.</h1>
+        <h2 className={styles.heading2}>I Build things for the web.</h2>
         <p className={styles.heroText}>A student specializing in building <em><span>full stack</span></em> web applications and experiences. I am currenlty focused on creating frontend user interfaces and backend systems with <em><span> professional styled</span></em> workflows.</p>
         <a
         className={`${styles.projects}  ${styles.button}`}
