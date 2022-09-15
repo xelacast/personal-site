7850ecbe9931ca36bdfd02fa5238632f295fba4b
Alexander Castillo <jhuiicebox@gmail.com>
2022-09-14 20:29:44 -0600

    Added right social fixed email

--- src/components/Socials.js
+++ src/components/Socials.js
@@ -5,19 +5,31 @@ import linkedIn from '../assets/icons/linkedin-square.svg';
 const Socials = () => {
   return (
     <section id={styles.socials}>
-      <div className={styles.socials}>
-        <div className={styles.line}></div>
-        <a href="https://github.com/alexjohn7516">
-          <ion-icon src={github}></ion-icon>
-        </a>
-        <a href="https://www.linkedin.com/in/alexander-castillo1/">
-          <ion-icon src={linkedIn}></ion-icon>
-        </a>
+      <div className={styles.linkContainer}>
+        <ul className={styles.links}>
+          <li className={styles.link}>
+            <a href="https://github.com/alexjohn7516">
+              <ion-icon src={github}></ion-icon>
+            </a>
+          </li>
+          <li className={styles.link}>
+            <a href="https://www.linkedin.com/in/alexander-castillo1/">
+            <ion-icon src={linkedIn}></ion-icon>
+            </a>
+          </li>
+        </ul>
       </div>
-      {/* <div className={styles.email}>
-        <div className={styles.line}></div>
-        <a href="mailto: alex.jc7516@gmail.com">alex.jc7516@gmail.com</a>
-      </div> */}
+      <div className={styles.emailContainer}>
+        <ul className={styles.links}>
+          <li className={styles.link}>
+            <a
+            href="mailto: alex.jc7516@gmail.com"
+            className={styles.email}>
+              alex.jc7516@gmail.com
+            </a>
+          </li>
+        </ul>
+      </div>
     </section>
   )
 }
