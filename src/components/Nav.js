c3fe36fb840d99a31105c55d98754f03738059a4
Alexander Castillo <jhuiicebox@gmail.com>
2022-09-15 15:51:52 -0600

    drop down scroll bar mobile

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,50 +1,81 @@
 import styles from "../styles/Nav.module.css"
+import burgerIcon from '../assets/icons/align-right.svg';
+import x from '../assets/icons/x.svg';
+import droplet from '../assets/icons/droplet.svg';
+import { useState } from "react";
 import { BrowserRouter, NavLink } from "react-router-dom";
 const Nav = () => {
+
+  const [menu, setMenu] = useState("hidden");
+  const [burger, setBurger] = useState("closed");
+
+  const toggleBurger = () => {
+    console.log(menu)
+    if (menu === "hidden") {
+      setMenu("visible");
+    } else {
+      setMenu("hidden");
+    }
+
+    if (burger === "closed") {
+      setBurger("opened");
+    } else {
+      setBurger("closed");
+    }
+  }
+
   return (
-    <section id={styles.nav} className={styles.nav}>
+    <section id="nav" className={styles.nav}>
       <BrowserRouter>
-        <div>
-          <nav className={styles.navBar}>
-            <ul className={styles.navLinks}>
-              <li className={styles.navLink}>
-                <NavLink to="/">
-                <span
-                className={`${styles.navLogo} ${styles.logo}`}
-                >Logo Here</span>
-                </NavLink>
-              </li>
-              <li className={styles.navLink}>
-                <NavLink
-                  to="#about"
-                  >
-                  <span className={styles.roman}>I.</span> About
-                </NavLink>
-              </li>
-              <li className={styles.navLink}>
-                <NavLink to="#projects">
-                  <span className={styles.roman}>II.</span> Projects
-                </NavLink>
-              </li>
-              <li className={styles.navLink}>
-                <NavLink
-                  to="#contact"
-                  >
-                  <span className={styles.roman}>III.</span> Contact
-                </NavLink>
-              </li>
-              <li className={styles.navLink}>
-                <NavLink
-                  to="resume.pdf"
-                  target="_blank"
-                  className={`${styles.resume} ${styles.button}`}
-                  >
-                  Resume
-                </NavLink>
-              </li>
-            </ul>
-          </nav>
-        </div>
+        <nav className={styles.container}>
+          <div className={styles.logo}>
+            <a href="#hero">
+              <ion-icon src={droplet}></ion-icon>
+            </a>
+          </div>
+          <div className={styles.menu}>
+            <div>
+              <button
+                onClick={toggleBurger}
+                className={`${styles.burger} ${burger}`}>
+                  <div>
+                    <ion-icon src={burgerIcon}></ion-icon>
+                  </div>
+                  {/* <div className={styles.x}>
+                    <ion-icon src={x}></ion-icon>
+                  </div> */}
+              </button>
+            </div>
+            <div className={`${menu} ${styles.linkContainer}`}>
+              <ul className={styles.links}>
+                <li className={styles.link}>
+                  <NavLink to="#about">
+                    <span className={styles.roman}>I.</span> About
+                  </NavLink>
+                </li>
+                <li className={styles.link}>
+                  <NavLink to="#projects">
+                    <span className={styles.roman}>II.</span> Projects
+                  </NavLink>
+                </li>
+                <li className={styles.link}>
+                  <NavLink to="#contact">
+                    <span className={styles.roman}>III.</span> Contact
+                  </NavLink>
+                </li>
+                <li className={styles.link}>
+                  <NavLink
+                    to="resume.pdf"
+                    target="_blank"
+                    className={`${styles.resume} ${styles.button}`}
+                    >
+                    Resume
+                  </NavLink>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
       </BrowserRouter>
     </section>
   )
