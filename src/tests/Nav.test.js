3947273f12bb4d3a3f5346e67cd49aa7bd5f32b9
Alexander Castillo <jhuiicebox@gmail.com>
2022-09-18 14:33:07 -0600

    Testing Navigation Anchor Hrefs

--- src/tests/Nav.test.js
+++ src/tests/Nav.test.js
@@ -1,12 +1,39 @@
-import { render, fireEvent, screen} from '@testing-library/react';
+import { render, fireEvent, screen, cleanup} from '@testing-library/react';
 import Nav from '../components/Nav';
 
-it("navigates appropriatley", async () => {
+const resiseWindow = (x, y) => {
+  window.innerHeight = y;
+  window.innerWidth = x;
+  window.dispatchEvent(new Event("resize"));
+}
+
+beforeEach(() => {
+})
+
+it("Checks accuracy HREF of NAV links", () => {
+
   render(<Nav />);
 
-  const hero = await screen.findByTestId("hero");
+  const about = screen.getByText("About");
+  const projects = screen.getByText("Projects");
+  const contact = screen.getByText("Contact");
+  const resume = screen.getByText("Resume");
+  // const hero = screen.getByText("logo");
 
-  fireEvent.click(hero);
+  expect(about.getAttribute("href")).toEqual("#about");
+  expect(projects.getAttribute("href")).toEqual("#projects");
+  expect(contact.getAttribute("href")).toEqual("#contact");
+  expect(resume.getAttribute("href")).toEqual("resume.pdf");
+  // expect(hero.getAttribute("href")).toEqual("#hero");
 
-  // expect()
+  cleanup();
+
+})
+
+test("Tests mobile navigation menu.", () => {
+  render(<Nav />);
+
+  resiseWindow(750, 1000);
+
+  const clickable = screen.getByRole('button', {name: "burger"});
 })
