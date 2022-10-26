97f15685c96bce7379174c0c40c3cc69c09ee78d
Alexander Castillo <jhuiicebox@gmail.com>
2022-10-26 15:12:38 -0600

    changed content to more projects

--- src/assets/content/moreProjects.js
+++ src/assets/content/moreProjects.js
@@ -1,32 +1,32 @@
 export const moreProjects = [
   {
     title: "Social Media",
-    description: "A full stack social media application built with Python, Django, SQLite, HTML5, CSS3",
+    description: "Social media application built with Python, Django, SQLite, HTML5, CSS3",
     url: "https://github.com/alexjohn7516/mini-social-media",
   },
   {
     title: "Coming Soon",
-    description: "A fancy projects for you coming soon",
+    description: "Project coming soon",
     url: "",
   },
   {
     title: "Coming Soon",
-    description: "A fancy projects for you coming soon",
+    description: "Project coming soon",
     url: "",
   },
   {
     title: "Coming Soon",
-    description: "A fancy projects for you coming soon",
+    description: "Project coming soon",
     url: "",
   },
   {
     title: "Coming Soon",
-    description: "A fancy projects for you coming soon",
+    description: "Project coming soon",
     url: "",
   },
   {
     title: "Coming Soon",
-    description: "A fancy projects for you coming soon",
+    description: "Project coming soon",
     url: "",
   }
 ]
