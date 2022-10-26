883c2cac0d77df54e9c7f11a6f5205deb5f4b78f
Alexander Castillo <jhuiicebox@gmail.com>
2022-10-26 14:00:05 -0600

    fixed typo in hero

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -7,7 +7,7 @@ const Hero = () => {
         <p className={styles.intro}>Hi, My Name is...</p>
         <h1 className={styles.heading}>Alexander Castillo</h1>
         <h2 className={styles.heading2}>I create software for the web.</h2>
-        <p className={styles.heroText}>Who specializes in building <em><span>full stack</span></em> web applications and for small businesses. I am currenlty focused on integrating <em><span>TypeScript</span></em> and project management with Jira services into my workflows.</p>
+        <p className={styles.heroText}>Who specializes in building <em><span>full stack</span></em> web applications for small businesses. I am currenlty focused on integrating <em><span>TypeScript</span></em> and project management with Jira services into my workflows.</p>
         <a
         className={`${styles.projects}  ${styles.button}`}
         href="https://www.github.com/alexjohn7516"
