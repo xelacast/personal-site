916772f405857f579cb9b3d3a38691a0e4b24a81
Alexander Castillo <jhuiicebox@gmail.com>
2022-10-26 14:51:53 -0600

    removed typos from about me

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -22,25 +22,21 @@ const AboutMe = () => {
     <section id="about">
       <div className={styles.container}>
         <div className={styles.heading}>
-          <h3><span>Iw.</span> About Me </h3>
+          <h3><span>I.</span> About Me </h3>
         </div>
         <div className={styles.body}>
           <div className={styles.description}>
             <div className={styles.text}>
               <p>I am a self-taught software enthusiast creating frontend and
-                full-stack products with a years worth of freelance development
-                experience.
+                full-stack products.
               </p>
-              {/* <p>My enjoyment for web development started when
-                I took an online python EDx course
-                to get ahead in school, I was hooked after that.</p> */}
               <p>
                 My main focus is to learn industry standards and project workflows
                 to build career worthy skills. I have <span>experience</span> in
                 creating <span>full stack applications</span> from the ground up
-                in the Blockchain space and with the <span>MERN</span> Stack.</p>
+                in the Blockchain and currently SASS space through freelance work.</p>
               <p>My goal is to secure a career as a fullstack software developer
-                with an innovative company and further down the road create my own business
+                with an innovative company and build a personal brand in the process.
               </p>
               <p>These are a few technologies I have been working with recently.</p>
             </div>
